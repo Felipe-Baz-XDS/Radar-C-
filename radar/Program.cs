3f24b4a1086bfa578eb6b716a338d03048a93cd0
felipe <baz@xds.com.br>
2021-10-21 14:22:24 -0300

    Adding Radar function

---
+++ radar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace radar
+{
+    class Program
+    {
+        public static void Radar()
+        {
+            Console.WriteLine("Qual a velocidade do veiculo:\n");
+            int velocidade = int.Parse(Console.ReadLine());
+
+            if(velocidade > 80)
+            {
+                int valor = ((velocidade - 80)*5); 
+
+                string msg = String.Format("Você foi multado em R{0,2:C}", valor);
+                Console.WriteLine(msg);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Radar();
+        }
+    }
+}
